6f73a54e27e04a04f6049573f0adf1686231d0a1
propython <94940763+propytho@users.noreply.github.com>
2023-12-05 18:31:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Set the date we're counting down to
+    const countDownDate = new Date("Dec 25, 2023 00:00:00").getTime();
 
+    // Update the countdown every 1 second
+    const x = setInterval(function () {
+        // Get the current date and time
+        const now = new Date().getTime();
+
+        // Calculate the remaining time
+        const distance = countDownDate - now;
+
+        // Calculate days, hours, minutes, and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the countdown
+        document.getElementById("timer").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        // If the countdown is over, display a message
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "Merry Christmas!";
+        }
+    }, 1000);
+});
