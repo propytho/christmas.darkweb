f579a114e4e9041b711ef2d9a5c79e020fd2afec
propython <94940763+propytho@users.noreply.github.com>
2023-12-05 18:01:10 +0700

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// You can add additional JavaScript for decorating the globe here.
-// For simplicity, this example doesn't include any additional decorations.
 
-// You may use Three.js or another library for a more advanced 3D globe.
-
-// Example: Adding a snow effect to the background
-document.body.style.backgroundImage = "url('snow.jpg')";
